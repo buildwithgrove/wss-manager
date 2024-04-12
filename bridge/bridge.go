10acc95c4b42683883e5ac1299f9e03205ab1c19
commoddity <pascal@pokt.network>
2024-04-12 12:06:03 +0100

    feat: add bridge package and unit tests

---
+++ bridge/bridge.go
@@ -0,0 +1,94 @@
+package bridge
+
+import (
+	"github.com/google/uuid"
+	"github.com/pokt-foundation/portal-http-db/v2/types"
+	"github.com/pokt-foundation/utils-go/logger"
+)
+
+type (
+	// Bridge routes data between clients and servers
+	Bridge struct {
+		id    uuid.UUID
+		app   types.PortalAppID
+		chain types.ChainAlias
+
+		clientConn  wsConnection
+		gatewayConn wsConnection
+
+		log *logger.Logger
+
+		// TODO - add subscription map for the bridge
+	}
+
+	Builder struct {
+		log *logger.Logger
+	}
+
+	wsConnection interface {
+		ReadMessage() (messageType int, p []byte, err error)
+		WriteMessage(messageType int, data []byte) error
+	}
+)
+
+func NewBuilder(log *logger.Logger) *Builder {
+	return &Builder{
+		log: log,
+	}
+}
+
+func (b *Builder) NewBridge(app types.PortalAppID, chain types.ChainAlias, clientConn, gatewayConn wsConnection) *Bridge {
+	return &Bridge{
+		id:          uuid.New(),
+		app:         app,
+		chain:       chain,
+		clientConn:  clientConn,
+		gatewayConn: gatewayConn,
+		log:         b.log,
+	}
+}
+
+// Run starts the bridge and establishes a bidirectional communication between the client and server
+func (b *Bridge) Run() {
+	// Start goroutine to read from client and write to gateway
+	go func() {
+		for {
+			messageType, message, err := b.clientConn.ReadMessage()
+			if err != nil {
+				b.log.Error("Error reading from client websocket:", err)
+			}
+
+			// TODO - intercept `eth_subscription` messages and set the custom ID on them
+
+			err = b.gatewayConn.WriteMessage(messageType, message)
+			if err != nil {
+				b.log.Error("Error writing to gateway websocket:", err)
+			}
+		}
+	}()
+
+	// Start goroutine to read from gateway and write to client
+	go func() {
+		for {
+			messageType, message, err := b.gatewayConn.ReadMessage()
+			if err != nil {
+				b.log.Error("Error reading from gateway websocket:", err)
+			}
+
+			// TODO - intercept `eth_subscription` message replies, match the ID from the send step and add to the subscriptions map for this bridge
+
+			err = b.clientConn.WriteMessage(messageType, message)
+			if err != nil {
+				b.log.Error("Error writing to client websocket:", err)
+			}
+		}
+	}()
+}
+
+func (b *Bridge) Chain() types.ChainAlias {
+	return b.chain
+}
+
+func (b *Bridge) App() types.PortalAppID {
+	return b.app
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module github.com/pokt-foundation/wss-manager
 
 go 1.22.0
+
+replace github.com/cosmos/cosmos-sdk => github.com/rollkit/cosmos-sdk v0.47.3-rollkit-v0.10.6-no-fraud-proofs
+
+replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/pokt-foundation/portal-http-db/v2 v2.15.0
+	github.com/pokt-foundation/utils-go v0.11.1
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
