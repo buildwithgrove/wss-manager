a72d6edf12a43dfb8a0a36d8f2ef13ffd606a577
commoddity <pascal@pokt.network>
2024-06-20 18:17:44 +0100

    fix: bridge unit test

--- bridge/bridge_test.go
+++ bridge/bridge_test.go
@@ -373,12 +373,11 @@ func Test_reconnectToGateway(t *testing.T) {
 			// Create a mock server for client connection
 			clientServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				upgrader := websocket.Upgrader{}
-				conn, err := upgrader.Upgrade(w, r, nil)
+				_, err := upgrader.Upgrade(w, r, nil)
 				if err != nil {
 					t.Error("Error during connection upgradation:", err)
 					return
 				}
-				conn.Close()
 			}))
 			defer clientServer.Close()
 
@@ -389,12 +388,11 @@ func Test_reconnectToGateway(t *testing.T) {
 			// Create a mock server for gateway connection
 			gatewayServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				upgrader := websocket.Upgrader{}
-				conn, err := upgrader.Upgrade(w, r, nil)
+				_, err := upgrader.Upgrade(w, r, nil)
 				if err != nil {
 					t.Error("Error during connection upgradation:", err)
 					return
 				}
-				conn.Close()
 			}))
 			defer gatewayServer.Close()
 
